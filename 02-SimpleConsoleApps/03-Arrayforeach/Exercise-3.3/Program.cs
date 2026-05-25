f5c22d33eb940bf3a16c781d6aa20dec2d32ae93
Sathya Narayanan M <sathyanarayanan1107@gmail.com>
2026-05-25 15:15:08 +0530

    Completed Module 2: Exercise 3.1, 3.2, 3.3

---
+++ 02-SimpleConsoleApps/03-Arrayforeach/Exercise-3.3/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] fraudelentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in fraudelentOrderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
