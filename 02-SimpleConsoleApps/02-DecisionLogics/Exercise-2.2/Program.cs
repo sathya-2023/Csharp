8c833926527ff0ec6e70960e6399cf7f48e39c1c
Sathya Narayanan M <sathyanarayanan1107@gmail.com>
2026-05-25 14:10:42 +0530

    Completed Module 2: Exercise 2.1, 2.2, 2.3

---
+++ 02-SimpleConsoleApps/02-DecisionLogics/Exercise-2.2/Program.cs
@@ -0,0 +1,48 @@
+﻿// You'll use the Random.Next() method to simulate rolling three six-sided dice. You'll evaluate the rolled values to calculate the score. If the score is greater than an arbitrary total, then you'll display a winning message to the user. If the score is below the cutoff, you'll display a losing message to the user.
+
+// If any two dice you roll result in the same value, you get two bonus points for rolling doubles.
+// If all three dice you roll result in the same value, you get six bonus points for rolling triples.
+// If the sum of the three dice rolls, plus any point bonuses, is 15 or greater, you win the game. Otherwise, you lose.
+
+// Code starts from here
+Random dice = new Random();
+
+int dice1 = dice.Next(1,7);
+int dice2 = dice.Next(1,7);
+int dice3 = dice.Next(1,7);
+
+int total = dice1 + dice2 + dice3;
+
+Console.WriteLine($"You rolled {dice1}, {dice2}, and {dice3} for a total of {total}.");
+
+if (dice1 == dice2 || dice1 == dice3 || dice2 == dice3)
+{
+    if (dice1 == dice2 && dice2 == dice3)
+    {
+        total += 6;
+        Console.WriteLine("You rolled triples! Bonus 6 points!");
+    }
+    else
+    {
+        total += 2;
+        Console.WriteLine("You rolled doubles! Bonus 2 points!");
+    }
+}
+if (total >= 16)
+{
+    Console.WriteLine("You won a Porsche 911 Turbo S!");
+}
+else if (total >=10 && total < 16)
+{
+    Console.WriteLine("You won a Tesla Model S Plaid!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip to Netherlands!");
+}
+else
+{
+    Console.WriteLine("You won a bicycle!");
+}
+
+
---
+++ 02-SimpleConsoleApps/02-DecisionLogics/Exercise-2.3/Program.cs
@@ -0,0 +1,53 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+// if (daysUntilExpiration <= 10)
+// {
+//     if (daysUntilExpiration <= 5)
+//     {
+//         if (daysUntilExpiration == 1)
+//         {
+//             Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//             Console.WriteLine($"Renew now and save 20%!");
+//         }
+//     else
+//     {
+//         Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//         Console.WriteLine($"Renew now and save 10%!");
+//         //     discountPercentage = 10;
+//     }
+
+//     }
+//     else
+//     {
+//         Console.WriteLine("Your subscription will expire soon. Renew now!");
+//     }
+// }
+
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} day.");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
